a5d105719c43cc4fc9c0457caa4948f834ebb05b
kentin <kentin.chen@gmail.com>
2026-07-18 23:04:08 +0800

    优化

--- cpolar-tunnel/run.ps1
+++ cpolar-tunnel/run.ps1
@@ -1,2 +1,4 @@
+python cpolar-tunnel/scripts/cpolar_ssh_public_url.py
+python cpolar-tunnel/scripts/dynamic_proxy_script.py (python cpolar-tunnel/scripts/cpolar_ssh_public_url.py | Select-String tcp).Line.Trim() 2001
 python cpolar-tunnel/scripts/twin_dynamic_proxy_script.py (python cpolar-tunnel/scripts/cpolar_ssh_public_url.py | Select-String tcp).Line.Trim()
 netstat -ano|findstr "20808 20809"
